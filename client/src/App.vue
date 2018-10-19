f2c19f611cb83e650a8220f91997d9c63f7f4e38
[Copelia] <iamcorallium@gmail.com>
2018-10-18 22:35:16 -0500

    Añadiendo componente

---
+++ client/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <v-toolbar fixed color="secondary">
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor:pointer">
+          Burger Cuin
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in navItem" :key="item.title" :to="item.link">
+          <v-icon class="hidden-sm-only" left >{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+
+      </v-toolbar-items>
+
+    </v-toolbar>
+    <!--App content-->
+    <main>
+      <router-view/>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+    navItem() {
+      return [
+        {icon: 'create', title:'Registro', link: '/signUp'},
+        {icon: 'lock_open', title:'Inicia sesión', link: '/signIn'}
+      ]
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-btn color="warning"><v-icon>search</v-icon></v-btn>
+    <router-view/>
+
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
