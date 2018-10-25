d3effc352524a8de894f51f3de104640f555d745
[Copelia] <iamcorallium@gmail.com>
2018-10-24 23:57:02 -0500

    Guardando token en local storage

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { defaultClient as ApolloClient } from './main'
-import { gql } from 'apollo-boost';
+// import { gql } from 'apollo-boost';
 import { SIGNIN_USER } from './queries';
-import { log } from 'util';
+// import { log } from 'util';
 
 Vue.use(Vuex)
 
